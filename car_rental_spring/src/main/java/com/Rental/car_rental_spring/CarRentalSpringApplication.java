ea14724393b6f38d2ca705e6a824b06bad59745e
jonastoussaint <jonas.toussaint91@gmail.com>
2024-04-29 21:45:55 -0400

    springboot tutorial

---
+++ car_rental_spring/src/main/java/com/Rental/car_rental_spring/CarRentalSpringApplication.java
@@ -0,0 +1,13 @@
+package com.Rental.car_rental_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalSpringApplication.class, args);
+	}
+
+}
---
+++ car_rental_spring/src/test/java/com/Rental/car_rental_spring/CarRentalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rental.car_rental_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
